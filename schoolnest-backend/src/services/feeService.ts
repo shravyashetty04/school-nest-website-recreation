1fcab02bb7ea732d495a978d1105d878a23cd398
Shravya <shravyashetty415@gmail.com>
2026-09-17 18:14:23 +0530

    Remove backend from repo

--- schoolnest-backend/src/services/feeService.ts
+++
@@ -1,5 +0,0 @@
-import { FeeInvoice, IFeeInvoice } from '../models/FeeInvoice';
-
-export const getInvoices = async () => {
-  return await FeeInvoice.find({}).populate('studentId', 'firstName lastName admissionNumber');
-};
